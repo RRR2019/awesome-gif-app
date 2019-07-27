41540b08a9199857f7e3770b75de7a8bd74325e9
Rodrigo Robles <rrobles.rodrigo@gmail.com>
2019-07-27 13:22:45 -0400

    basic javascript

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,3 +1,45 @@
 var gifList = [];
 
-function createImg(response) {}
+function createImg(response) {
+  for (i = 0; i < gifList.length; i++) {
+    var gif = $("<img>");
+    gif.attr("src", response.data.original.url);
+    gif.attr("alt", response.data, slug + " Gif");
+
+    var image = $("<div>");
+    image.append(gif);
+    $(".col-md-12").append(image); // to show the image in body
+  }
+}
+
+for (var i = 0; i < gifList.length; i++) {
+  var imgGif = getGif(imgGif);
+}
+
+function getGif(gifSearch) {
+  // queryURL endpoint for OMDB API
+  var queryURL =
+    "https://api.giphy.com/v1/gifs/search?q=" +
+    gifSearch +
+    "&api_key=A8zXa2FhTisNo4nWf63dDEFxlyhwUClg&limit=5";
+
+  // AJAX call to OBMD API with promise and callback handler
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).done(function(response) {
+    if (response.Response === "False") {
+      alert(response.Error);
+    } else {
+      console.log(response.data.id);
+      createImg(response);
+      gifList.push(response.data.id);
+    }
+  });
+}
+
+$("#search").click(function() {
+  var gifName = $("#gif-name").val();
+  console.log(gifName);
+  getGif(gifName);
+});
